017d31f6eb44139d63ab740a686bf84cb98051d6
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2023-04-10 22:36:58 -0300

    Update AddUsers.ps1
    correcao

--- AddUsers.ps1
+++ AddUsers.ps1
@@ -1,42 +1,49 @@
 $ErrorActionPreference = "Stop"
 
-$oi = Import-Csv '$dir\$arquivo.csv -Delimiter' ',' -PipelineVariable User
-ForEach ($l in $oi) {
+# Importa as linhas da planilha
+$importacao = Import-Csv '$dir\$arquivo.csv -Delimiter' ',' -PipelineVariable User
+ForEach ($l in $importacao) {
 
-$grupos = $l.'Group Member' -split ";"
+    # Armazena os nomes dos grupos
+    $grupos = $l.'Group Member' -split ";"
 
-$grupos1 = $grupos[0]
-$grupos2 = $grupos[1]
+    $grupos1 = $grupos[0]
+    $grupos2 = $grupos[1]
 
-if ($grupos1 -eq $null){
-    New-ADGroup -Name "$grupos1" -GroupScope Global  -Path "CN=Users,DC=lucaspolivira,DC=com"
-}
-if ($grupos2 -ne $null){
-$grupos3 = $grupos2
-if ($grupos3 -eq $null){
-    New-ADGroup -Name "$grupos3" -GroupScope Global  -Path "CN=Users,DC=lucaspolivira,DC=com"
-}}
+    # Ve se os grupos existem
+    if ($grupos1 -eq $null){
+        New-ADGroup -Name "$grupos1" -GroupScope Global  -Path "CN=Users,DC=lucaspolivira,DC=com"
+    }
 
-$securePassword = ConvertTo-SecureString -String $l.password -AsPlainText -Force
-$NewAdUserParameters = @{
-    Name = $l.'User logon name'
-    GivenName = $l.'First Name'
-    Surname = $l.'Last Name'
-    Department = $l.Department
-    Title = $l.'Job Title'
-    Homephone = $l.'Telephone Number'
-    Street = $l.Street
-    City = $l.City
-    Country = "Br"
-    logon = $l.'User logon name'
-    accountpassword = $securePassword
+    # Confere se o segundo grupo existe ou nao para criar
+    if ($grupos2 -ne $null){
+        $grupos3 = $grupos2
+        if ($grupos3 -eq $null){
+            New-ADGroup -Name "$grupos3" -GroupScope Global  -Path "CN=Users,DC=lucaspolivira,DC=com"
+        }
+    }
 
-}
+    # Adiciona os usuarios
+    $securePassword = ConvertTo-SecureString -String $l.password -AsPlainText -Force
+    $NewAdUserParameters = @{
+        Name = $l.'User logon name'
+        GivenName = $l.'First Name'
+        Surname = $l.'Last Name'
+        Department = $l.Department
+        Title = $l.'Job Title'
+        Homephone = $l.'Telephone Number'
+        Street = $l.Street
+        City = $l.City
+        Country = "Br"
+        logon = $l.'User logon name'
+        accountpassword = $securePassword
 
+    }
 
-new-AdUser @NewAdUserParameters
-Add-AdGroupMember -Identity $grupos[0] -Members $l.'User logon name'
-if ($grupos[1] -ne $null){
-Add-AdGroupMember -Identity $grupos[1] -Members $l.'User logon name'
-}
+    # Adiciona os usuarios aos grupos
+    new-AdUser @NewAdUserParameters
+    Add-AdGroupMember -Identity $grupos[0] -Members $l.'User logon name'
+        if ($grupos[1] -ne $null){
+        Add-AdGroupMember -Identity $grupos[1] -Members $l.'User logon name'
+        }
 }
